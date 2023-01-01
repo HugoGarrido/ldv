687b5e48cdfce474b2a199d89da946c26a2a542d
Hugo Garrido y Saez <garrido.saez.hugo@gmail.com>
2023-01-01 22:57:50 +0100

    feat: add general helper

--- packages/voodoo-ui/src/index.ts
+++ packages/voodoo-ui/src/index.ts
@@ -4,3 +4,8 @@ export { VoodooUIProvider } from './lib/VoodooUIProvider/VoodooUIProvider';
 export * from './lib/native-base/layout';
 export * from './lib/native-base/primitives';
 export * from './lib/native-base/hooks';
+export type {
+  VoodooUIResponsiveRecordValue,
+  VoodooUIResponsiveValue,
+} from './lib/utils/createResponsiveStyleFromProp/createResponsiveStyleFromProp';
+export { createResponsiveStyleFromProp } from './lib/utils/createResponsiveStyleFromProp/createResponsiveStyleFromProp';
---
+++ packages/voodoo-ui/src/lib/utils/createResponsiveStyleFromProp/createResponsiveStyleFromProp.ts
@@ -0,0 +1,37 @@
+import type { VoodooUIBreakpointName } from '../../theme/breakpoints';
+
+export type VoodooUIResponsiveRecordValue<T> = {
+  [key in VoodooUIBreakpointName]?: T;
+};
+
+export type VoodooUIResponsiveValue<T> =
+  | T
+  | VoodooUIResponsiveRecordValue<T>
+  | undefined;
+
+export function createResponsiveStyleFromProp<PossibleStyleValue>(
+  responsiveBooleanValue: VoodooUIResponsiveValue<boolean | undefined>,
+  valueIfTrue: PossibleStyleValue,
+  valueIfFalse?: PossibleStyleValue
+): VoodooUIResponsiveValue<PossibleStyleValue> {
+  // Handles simple boolean values
+  if (!responsiveBooleanValue) return valueIfFalse;
+
+  if (responsiveBooleanValue === true) return valueIfTrue;
+
+  // Handles responsive value object
+  const keys = Object.keys(responsiveBooleanValue) as VoodooUIBreakpointName[];
+
+  if (keys.length === 0) return valueIfFalse;
+
+  const responsiveValue =
+    {} as VoodooUIResponsiveRecordValue<PossibleStyleValue>;
+
+  keys.forEach((breakpointName) => {
+    responsiveValue[breakpointName] = responsiveBooleanValue[breakpointName]
+      ? valueIfTrue
+      : valueIfFalse;
+  });
+
+  return responsiveValue;
+}
